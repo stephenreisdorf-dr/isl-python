3b4e890efeaf22240efa0ce2453e59ad11d0cdbe
Stephen Reisdorf <60190981+stephenreisdorf-dr@users.noreply.github.com>
2021-10-08 13:16:02 -0500

    Cleaning up some comments and such.

--- 03_Linear_Regression/3.6.2.py
+++ 03_Linear_Regression/3.6.2.py
@@ -1,8 +1,3 @@
-# 3.6.1 (Only some libraries!)
-# Libraries will be imported as needed so it's clear what they're for
-# I would normally put my imports all at the top...
-# ... but it makes sense to spread them help understand the code!
-
 # 3.6.2
 # Import the libraries needed to load the BOSTON dataset 
 from sklearn.datasets import load_boston
