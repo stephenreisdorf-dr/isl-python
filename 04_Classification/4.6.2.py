0f876692fb5985e4322e6cfd29d2ec8fb0f1b7ba
Stephen Reisdorf <60190981+stephenreisdorf-dr@users.noreply.github.com>
2021-11-12 09:47:55 -0600

    Finishing 4.6.2

--- 04_Classification/4.6.2.py
+++ 04_Classification/4.6.2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from io import StringIO
 import statsmodels.api as sm
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score
 
 # Read Smarket data from some guys repo
 smarket_url = 'https://raw.githubusercontent.com/JWarmenhoven/ISLR-python/master/Notebooks/Data/Smarket.csv'
@@ -26,10 +27,70 @@ print(results.summary())
 print(results.params)
 
 # Display first 10 predictions
-results.predict(X)[:10]
+print(results.predict(X)[:10])
 
 y_pred = results.predict(X) >= .5
 y_pred_map = y_pred.map(lambda x: 'Up' if x else 'Down')
 
 # Display confusion matrix
-print(results.pred_table())
+print(confusion_matrix(y, y_pred))
+
+# Calculate training error rate
+print(accuracy_score(y, y_pred))
+
+# Retrain the model on a subset of the data
+X_train = sm.add_constant(
+    smarket_df[ smarket_df['Year'] < 2005 ]
+    .drop([ 'Today', 'Direction', 'Year' ], axis='columns')
+)
+y_train = (
+    smarket_df
+    [ smarket_df['Year'] < 2005 ]['Direction']
+    .map(lambda x: 1 if x == 'Up' else 0)
+)
+
+X_test = sm.add_constant(
+    smarket_df[ smarket_df['Year'] >= 2005 ]
+    .drop([ 'Today', 'Direction', 'Year' ], axis='columns')
+)
+y_test = (
+    smarket_df
+    [ smarket_df['Year'] >= 2005 ]['Direction']
+    .map(lambda x: 1 if x == 'Up' else 0)
+)
+model = sm.Logit(y_train, X_train)
+results = model.fit()
+
+y_pred = results.predict(X_test) >= 0.5
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# Retrain the model on a subset of the data with just Lag1 and Lag2
+X_train = sm.add_constant(
+    smarket_df[ smarket_df['Year'] < 2005 ]
+    [['Lag1', 'Lag2']]
+)
+y_train = (
+    smarket_df
+    [ smarket_df['Year'] < 2005 ]['Direction']
+    .map(lambda x: 1 if x == 'Up' else 0)
+)
+
+X_test = sm.add_constant(
+    smarket_df[ smarket_df['Year'] >= 2005 ]
+    [['Lag1', 'Lag2']]
+)
+y_test = (
+    smarket_df
+    [ smarket_df['Year'] >= 2005 ]['Direction']
+    .map(lambda x: 1 if x == 'Up' else 0)
+)
+model = sm.Logit(y_train, X_train)
+results = model.fit()
+
+y_pred = results.predict(X_test) >= 0.5
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred))
+
+results.predict(sm.add_constant([[1.2,1.5],[1.1,-0.8]]))
